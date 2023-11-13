7da6df58021da7cbf0cc8c003ce4f1712cccc177
yujin <ps5045@naver.com>
2023-11-13 17:30:12 +0900

    feat(user): Add user-info API

--- routers/users.router.js
+++ routers/users.router.js
@@ -1,13 +1,14 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-const { Users } = require("../models");
-const createError = require("../utils/errorResponse");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const { Users } = require('../models');
+const createError = require('../utils/errorResponse');
+const authenticate = require('../middleware/need-signin.middleware');
 
-require("dotenv").config();
+require('dotenv').config();
 
-router.post("/auth/login", async (req, res, next) => {
+router.post('/auth/login', async (req, res, next) => {
   const { email, password } = req.body;
 
   try {
@@ -15,7 +16,7 @@ router.post("/auth/login", async (req, res, next) => {
     const token = generateToken(user.id);
 
     return res.status(200).json({
-      message: "로그인에 성공했습니다.",
+      message: '로그인에 성공했습니다.',
       data: {
         ...token,
         userId: user.id,
@@ -27,7 +28,7 @@ router.post("/auth/login", async (req, res, next) => {
 });
 
 const generateToken = (userId) => {
-  const expiresIn = "12h";
+  const expiresIn = '12h';
   const accessToken = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn,
   });
@@ -38,22 +39,26 @@ const handleLogin = async (email, password) => {
   const user = await Users.findOne({ where: { email } });
 
   if (!user) {
-    throw createError(
-      400,
-      "해당 이메일을 가진 사용자를 찾을 수 없습니다."
-    );
+    throw createError(400, '해당 이메일을 가진 사용자를 찾을 수 없습니다.');
   }
 
-  const isValidPassword = await bcrypt.compare(
-    password,
-    user.password
-  );
+  const isValidPassword = await bcrypt.compare(password, user.password);
 
   if (!isValidPassword) {
-    throw createError(400, "비밀번호가 일치하지 않습니다.");
+    throw createError(400, '비밀번호가 일치하지 않습니다.');
   }
 
   return user;
 };
+
+router.get('/auth/me', authenticate, async (req, res, next) => {
+  try {
+    const user = res.locals.user;
+    const { id, email, name } = user;
+    res.json({ id, email, name });
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
